9a7054ca7d2bacfaf0d69f4a1a878ac4807b6d08
WeatherBoy <Dafloxi@gmail.com>
2022-11-13 12:22:39 +0100

    Update data_preprocessor.py

--- data_processing/data_preprocessor.py
+++ data_processing/data_preprocessor.py
@@ -2,20 +2,52 @@ import json
 import os
 import numpy as np
 
+# TODO:
+# - Check whether the amount of saved messages & responses makes sense
+#       (right now it looks like there are duplicates)
+# - Save the file as something we can use in a dataloader
+
+
 # I had the JSON file in a neighbouring directory I called data:
 # print(f"Current working directory: {os.getcwd()}") # for seeing working directory
 DATA_PATH = "data/reddit_casual.json"
+NEW_DATA_PATH = "data/processed_reddit_casual.txt"
+TESTING = True
 
 # Opening JSON file
 f = open(DATA_PATH)
   
 # returns JSON object as a dictionary
 data = json.load(f)
-data = data[:10] # for testing
+
+if TESTING:
+    # ingenius code... yes I know
+    data = data[:10]
+    
 new_data = []
 
-# lambda function for processing string
+
+def stringProcessing(string_to_process : str):
+    import re
+    
+    # Removes those unicode-smileys
+    string_to_process = string_to_process.encode('ascii', 'ignore').decode('ascii')
+    
+    # This removes special characters i.e. quotation marks and smileys, but sadly also
+    # parentheses.
+    # What it basicly says is:
+    # 'anything that isn't:
+    #   a letter, a capital letter, a number, an exclamation mark, a question mark,
+    #   an apostrophe, a comma or a full stop
+    # replace it with a space'
+    string_to_process = re.sub(r"[^a-zA-Z0-9!?',.]+", ' ', string_to_process)
+    
+    return string_to_process
+
+
+# lambda function for processing string, removes quotation marks and unicode-smileys
 stringProcessing = lambda x : x.replace('\"', "").encode('ascii', 'ignore').decode('ascii')
+  
   
 # Iterating through the json list
 for i in data:
@@ -41,17 +73,19 @@ for i in data:
 
     for l in range(len(new_structure)-1):
        new_data.append([new_structure[l], new_structure[l+1]]) 
+
        
-#print("New data: ",new_data)
-for indx, elem in enumerate(new_data):
-    print(f"\n\nPair number: {indx}")
-    print("Message: ")
-    print(elem[0])
-    print("\nResponse: ")
-    print(elem[1])
+if TESTING:
+    # once more, ingenious testing
+    for indx, elem in enumerate(new_data):
+        print(f"\n\nPair number: {indx}")
+        print("Message: ")
+        print(elem[0])
+        print("\nResponse: ")
+        print(elem[1])
 
 new_data = np.array(new_data)
-print(new_data.shape)
+print(f"shape of new data: {new_data.shape}")
   
 # Closing file
 f.close()
